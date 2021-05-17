e9f3019b50c437ccbd8018b0c0978311d7f4db41
neethu-chandran <meeneethuu1995@gmail.com>
2021-05-18 02:21:11 +0530

    firstcommit

---
+++ assignment/js/script.js
@@ -0,0 +1,5 @@
+function add() {
+    document.getElementById("flower2").innerHTML = "<img src='../img/flower2.jpg'>"
+    document.getElementById("flower1").style = "display:none"
+
+}
---
+++ js/demo.js
@@ -0,0 +1,45 @@
+// function openDiv() {
+//     document.getElementById("div1")
+//     document.getElementById("div1").style.display = "block"
+//     document.getElementById("div1").style.fontsize = "70px";
+
+// }
+
+
+// function addContent() {
+//     document.getElementById("p1").innerHTML = "sdsad <span style='color:red'>yellow</span>";
+// }
+
+// function getData() {
+
+//     document.getElementById("username").value = "neethu"
+// }
+
+
+function add() {
+    let num1 = document.getElementById("value1").value
+    let num2 = document.getElementById("value2").value
+    document.getElementById("result").value = parseFloat(num1) + parseFloat(num2)
+}
+
+// let str = "123"
+// let i = parseInt(str)
+// console.log(i, typeof i)
+// let d = i.toString()
+// console.log(d, typeof d)
+
+
+
+// let a = "hello"
+// console.log(a, typeof a)
+// let a1 = String("hello")
+// console.log(a1, typeof a1)
+
+// let s = "hello"
+
+// console.log(s + "world")
+
+// let obj = new String("hello")
+// console.log(obj, typeof obj)
+// let obj1 = new String(123)
+// console.log(obj1, typeof obj1)
